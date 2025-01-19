b99ca829d1fd3a2a95a4fe1118ef5330d87ca07a
Philipp Polterauer <philipp.polterauer@gmail.com>
2025-01-19 10:39:56 +0100

    day16 part2 done!

--- src/bin/day16.rs
+++ src/bin/day16.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap, VecDeque};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 use adventofcode2024::{
     matrix::{Direction, FromChar, Matrix, MatrixIdx, MatrixIdxOffset, ALL_DIRECTIONS},
@@ -41,7 +41,7 @@ enum Input {
 const ALL_INPUTS: [Input; 3] = [Input::TurnLeft, Input::TurnRight, Input::Step];
 #[derive(PartialEq, Hash, Eq, Clone, Copy, Debug)]
 struct State(MatrixIdx, Direction);
-fn backward(state: &State, input: Input) -> (State, usize) {
+fn backward(state: &State, input: &Input) -> (State, usize) {
     let State(pos, dir) = state;
     match input {
         Input::TurnLeft => (State(*pos, dir.right()), 1000),
@@ -49,11 +49,15 @@ fn backward(state: &State, input: Input) -> (State, usize) {
         Input::Step => (State(pos - offset(dir), *dir), 1),
     }
 }
-
-fn part1(content: &str) -> usize {
-    let grid = Matrix::<Tiles>::try_from_str(content).unwrap();
-    let solution = 0;
-    let start = grid.find(&Tiles::Start).unwrap();
+fn forward(state: &State, input: &Input) -> (State, usize) {
+    let State(pos, dir) = state;
+    match input {
+        Input::TurnLeft => (State(*pos, dir.left()), 1000),
+        Input::TurnRight => (State(*pos, dir.right()), 1000),
+        Input::Step => (State(pos + offset(dir), *dir), 1),
+    }
+}
+fn compute_costmap(grid: &Matrix<Tiles>) -> HashMap<State, usize> {
     let end = grid.find(&Tiles::End).unwrap();
 
     // find possible end states and insert them into costmap with 0
@@ -71,7 +75,7 @@ fn part1(content: &str) -> usize {
 
     while let Some((state, total_cost)) = front.pop_front() {
         for input in ALL_INPUTS {
-            let (next_state, cost) = backward(&state, input);
+            let (next_state, cost) = backward(&state, &input);
             let new_total_cost = total_cost + cost;
             if let Some(Tiles::Free | Tiles::Start) = grid.get(&next_state.0) {
                 cost_map
@@ -89,11 +93,34 @@ fn part1(content: &str) -> usize {
             }
         }
     }
-    cost_map[&State(start, Direction::Right)]
+    cost_map
 }
-fn part2(content: &str) -> i64 {
-    let solution = 0;
-    solution
+fn part1(content: &str) -> usize {
+    let grid = Matrix::<Tiles>::try_from_str(content).unwrap();
+    let start = State(grid.find(&Tiles::Start).unwrap(), Direction::Right);
+    let cost_map = compute_costmap(&grid);
+    cost_map[&start]
+}
+
+fn part2(content: &str) -> usize {
+    let grid = Matrix::<Tiles>::try_from_str(content).unwrap();
+    let start = State(grid.find(&Tiles::Start).unwrap(), Direction::Right);
+    let costmap = compute_costmap(&grid);
+    let mut front = VecDeque::from([(start, costmap[&start])]);
+    let mut visited = HashSet::new();
+    while let Some((state, cost_to_go)) = front.pop_front() {
+        for input in ALL_INPUTS {
+            let (nstate, cost) = forward(&state, &input);
+            if let Some(remaining) = costmap.get(&nstate) {
+                // here we have optimal route found
+                if *remaining == cost_to_go - cost {
+                    front.push_back((nstate, *remaining));
+                }
+            }
+        }
+        visited.insert(state.0);
+    }
+    visited.len()
 }
 
 fn main() {
@@ -107,7 +134,56 @@ fn main() {
 
 #[cfg(test)]
 mod test {
+    use crate::{part1, part2};
+
+    const TEST1: &str = "###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############";
+
+    const TEST2: &str = "#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################";
 
     #[test]
-    fn test_1() {}
+    fn test_part1_1() {
+        assert_eq!(part1(TEST1), 7036);
+    }
+    #[test]
+    fn test_part1_2() {
+        assert_eq!(part1(TEST2), 11048);
+    }
+    #[test]
+    fn test_part2_1() {
+        assert_eq!(part2(TEST1), 45);
+    }
+    #[test]
+    fn test_part2_2() {
+        assert_eq!(part2(TEST2), 64);
+    }
 }
